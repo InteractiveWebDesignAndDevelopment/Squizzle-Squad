a879c541584e72e1671875852aad4590494e951b
Esat IBIS <esat.taha.ibis@gmail.com>
2016-10-18 15:13:48 -0400

    Inital Squizzle

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by esattahaibis on 2016-10-07.
+ */
+smoothScroll.init({
+    selector: '.smooth', // Selector for links (must be a class, ID, data attribute, or element tag)
+    selectorHeader: null, // Selector for fixed headers (must be a valid CSS selector) [optional]
+    speed: 800, // Integer. How fast to complete the scroll in milliseconds
+    easing: 'easeInOutCubic', // Easing pattern to use
+    offset: 0, // Integer. How far to offset the scrolling anchor location in pixels
+    callback: function ( anchor, toggle ) {} // Function to run after scrolling
+});
